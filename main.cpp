023e5f5314fb7d2ce0fdd83d5813aac6cd69c97f
tteeeoooo <147204778+tteeeoooo@users.noreply.github.com>
2024-04-01 16:45:02 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -177,7 +177,7 @@ public:
         return newPassword;
     };
 
-    static Account modificaDatele(Account user, const string fileName) {
+    static Account modificaDatele(Account user, const string &fileName) {
         string nume, parola, parolaNoua;
         nume = user.getName();
         parola = user.getPassword();
