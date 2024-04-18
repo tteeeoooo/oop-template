bf41a46bff1a4dc9371d6bdaab01e3c44fbc2671
tteeeoooo <147204778+tteeeoooo@users.noreply.github.com>
2024-04-18 14:35:57 +0300

    Create withalc.cpp

---
+++ src/withalc.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <exception>
+#include "drink.h"
+#include "noalc.h"
+#include "withalc.h"
+#include "cart.h"
+#include "account.h"
+#include "cout.h"
+
+using namespace std;
+
+
+
+WithAlc::WithAlc(const string &nume, const double &pret, const int &alc): Drink(nume, pret), alcohol(alc) {};
+
+// WithAlc(string nume, double pret, int alc): Drink(nume, pret), alcohol(alc) {};
+
+WithAlc::WithAlc(const WithAlc& notGood) = default;
+
+//void WithAlc::setAlc(int percent) {
+//    alcohol = percent;
+//}
+
+
+void WithAlc::description() const  {
+    cout << "This drink contains alcohol in a percentage of " << WithAlc::getAlc() << "%" << endl;
+}
+
+int WithAlc::getAlc() const {
+    return alcohol;
+}
+
+
+double WithAlc::priceModifier() const {
+    double p = price + (price * 19) / 100;
+    return p + p / 20;
+}   //returneaza pretul cu tva + acciza de 5% pe bauturile alcoolice
+
+
+
+
+bool WithAlc::ageCheck() {
+    short int input;
+    cout << "Please enter your age: ";
+    cin >> input;
+    if (input < 21) {
+        return false;
+    }
+    else {
+        cout << "You can have access to alcoholic drinks!" << endl;
+        cout << "Side note: according to the Romanian regulations regarding selling fo alcoholic drinks, you will be imposed with a 5% excise tax!" << endl;
+        return true;
+    }
+}
+
+
+std::ostream& WithAlc::operator<<(std::ostream& coutt) {
+    coutt << drinkChoice << " " << price << alcohol;
+    return coutt;
+}
+
+
+std::istream& WithAlc::operator>>(std::istream& cinnn) {
+    cinnn >> drinkChoice >> price >> alcohol;
+    return cinnn;
+}
+
+
+
+WithAlc& WithAlc::operator=(const WithAlc& hydratingg) {
+    if (this != &hydratingg) {
+        drinkChoice = hydratingg.drinkChoice;
+        price = hydratingg.price;
+        alcohol = hydratingg.alcohol;
+    }
+    return *this;
+}
+
+
+//WithAlc::~WithAlc() = default;
