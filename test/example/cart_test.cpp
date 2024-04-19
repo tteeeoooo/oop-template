7873a354eefca440198f76ab9e8abf05fa3a51ec
tteeeoooo <147204778+tteeeoooo@users.noreply.github.com>
2024-04-19 18:22:24 +0300

    Update cart_test.cpp

--- test/example/cart_test.cpp
+++ test/example/cart_test.cpp
@@ -4,19 +4,22 @@
 
 TEST(CartTest, DefaultConstructor) {
     Cart cart;
-    EXPECT_EQ(cart.cartPrice(), 0.0f); // Verificăm dacă prețul coșului este 0
-    EXPECT_TRUE(cart.getDrinks(cart).empty()); // Verificăm dacă coșul este gol
+    EXPECT_EQ(cart.cartPrice(), 0.0f); 
+    EXPECT_TRUE(cart.getDrinks(cart).empty()); 
 }
 
 TEST(CartTest, ParameterizedConstructor) {
     std::vector<Drink*> drinks = {};
     std::vector<float> prices = {};
     Cart cart(drinks, 50.0f, prices);
-    EXPECT_EQ(cart.cartPrice(), 50.0f); 
+    EXPECT_EQ(cart.cartPrice(), 50.0f);
     EXPECT_EQ(cart.getDrinks(cart).size(), drinks.size()); 
 }
 
-TEST(CartTest, ProductDeleteTest) {}
+TEST(CartTest, ProductDeleteTest) {
+    Cart cart;
+}
 
-TEST(CartTest, CartPriceTest) {}
-
+TEST(CartTest, CartPriceTest) {
+    Cart cart;
+}
