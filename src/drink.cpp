d59e93518b71db6167ad314c4a638acdf91a26d8
tteeeoooo <147204778+tteeeoooo@users.noreply.github.com>
2024-04-19 10:55:48 +0300

    Update drink.cpp

--- src/drink.cpp
+++ src/drink.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <fstream>
-#include <exception>
 #include "drink.h"
 #include "noalc.h"
 #include "withalc.h"
@@ -12,104 +10,48 @@
 
 using namespace std;
 
-float Cout::priceCalculation(const float oldPrice, const float sale) {
-    return (oldPrice * sale) / 100;
+
+Drink::Drink(const std::string &drinkName, const double &priceTag): drinkChoice(drinkName), price(priceTag) {}
+
+Drink& Drink::operator=(const Drink &bauturica) {
+    if (this != &bauturica) {
+        this -> drinkChoice = bauturica.drinkChoice;
+        this -> price = bauturica.price;
+    }
+    return *this;
 }
 
 
+void Drink::description() const {};
 
-void Cout::orderAndExit() {
-    short int input;
-    cout <<endl << "Would you like to order your cart?" << endl;
-    cout << "Press 1 to confirm!" << endl;
-    cout << "Press 0 to exit! :(" << endl;
-    while (true) {
 
-        cin >> input;
-        try {
+double Drink::priceModifier() const {
+    return price;
+}
 
-            if (input == 1) {
-                Cout::coutFinishedOrder();
-                break;
-            }
-            else if (input == 0) {
-                Cout::coutAbortedOrder();
-                break;
-            }
-            else {
-                throw std::invalid_argument("Your choice is invalid. Please try again!");
-            }
-        }
 
-        catch (const std::exception& e) {
-            std::cerr << "Error: " << e.what() << std::endl;
-            cin.clear();
-        }
+std::ostream& Drink::operator<<(std::ostream& coutt) {return coutt;}
+
+
+string Drink::getDrinkName() {
+    return drinkChoice;
+}
+
+double Drink::getDrinkPrice() const{
+    return price;
+}
+
+
+void Drink::menuOptions(vector<Drink*> coffeeMenu) {
+    cout << endl << "Here are your options:" << endl;
+    for (int x = 0; x < int(coffeeMenu.size()); x++) {
+        cout << "Product " << x + 1 << ": " << coffeeMenu[x]->getDrinkName() << " - price with 19% TVA: " << coffeeMenu[x] -> priceModifier() << " " << endl;
     }
 }
 
+Drink::~Drink(){};
 
-
-
-void Cout::menuText() {
-    cout << "Choose the index of a product each time your want to add it to your cart! :D"<<endl;
-    cout << "If you want to delete a product from your cart, press '20'." << endl;
-    cout << "When you are done making your cart, just press 0! :)" << endl << endl;
-}
-
-
-
-int Cout::textToFinish() {
-    cout << endl << "Are you sure you don't want to edit your cart?" << endl;
-    cout << "Press 1 to go introduce your delivery information!" << endl;
-    cout << "Press 2 to go back to editing your cart!" << endl;
-    cout << "Press 0 to exit :(" << endl;
-    return 0;
-}
-
-
-
-void Cout::coutFinishedOrder() {
-    cout << "Thank you for your order! We are grinding your coffee right now, to make sure that you will have it as soon as possible! :)";
-}
-
-
-
-void Cout::coutAbortedOrder() {
-    cout << "We are sorry that you are leaving! See you next time!";
-}
-
-
-
-void Cout::todaysSales() {
-    cout << "Here is today's deal!" << endl;
-    underLine();
-    cout << "25% off for orders over $30" << endl;
-    upperLine();
-}
-
-
-
-void Cout::underLine() {
-    for (int i = 0; i < 30; i ++) {
-        cout << "_";
-    }
-    cout << endl;
-}
-
-
-
-void Cout::upperLine() {
-    for (int i = 0; i < 30; i ++) {
-        cout << "‾";
-    }
-    cout<<endl;
-}
-
-
-
-void Cout::exitText() {
-    cout << "Are you sure you want to exit the app?" << endl;
-    cout << "Press 1 to go back to order" << endl;
-    cout << "Press 0 to exit" << endl;
+double operator -(const Drink &bauturica, double procent) {
+    double reducere = (bauturica.getDrinkPrice() * procent) / 100;
+    return bauturica.getDrinkPrice() + reducere;
 }
