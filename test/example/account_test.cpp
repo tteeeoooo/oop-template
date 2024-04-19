4970f127ea0f4df20f02bc036007bd78d98232fd
tteeeoooo <147204778+tteeeoooo@users.noreply.github.com>
2024-04-19 19:33:06 +0300

    Update account_test.cpp

--- test/example/account_test.cpp
+++ test/example/account_test.cpp
@@ -1,42 +1,90 @@
-#include <gtest/gtest.h>
-#include "account.h"
+#include "account.h" 
+#include <gtest/gtest.h> 
+TEST(AccountTest, ConstructorTest) {
+    Account acc("JohnDoe", "password123");
+    EXPECT_EQ(acc.getName(), "JohnDoe");
+}
 
-TEST(AccountTest, DefaultConstructor) {
+TEST(AccountTest, CopyConstructorTest) {
+    Account original("JaneDoe", "qwerty");
+    Account copy(original);
+    EXPECT_EQ(copy.getName(), "JaneDoe");
+}
+
+TEST(AccountTest, GetNameTest) {
+    Account acc("Alice", "secret");
+    EXPECT_EQ(acc.getName(), "Alice");
+}
+
+TEST(AccountTest, ExcTest) {
     Account acc;
-    EXPECT_EQ(acc.getName(), ""); 
-}
-
-TEST(AccountTest, ParameterizedConstructor) {
-    Account acc("username", "password");
-    EXPECT_EQ(acc.getName(), "username"); 
-}
-
-TEST(AccountTest, UpgradeTest) {
-    Account acc("username", "password");
-    acc.upgrade();
+    std::stringstream input("1\n");
+    std::streambuf* old_cin = std::cin.rdbuf(input.rdbuf()); 
+    EXPECT_EQ(acc.exc(), 1);
+    std::cin.rdbuf(old_cin); 
 }
 
 TEST(AccountTest, UserReadTest) {
-    Account acc("username", "password");
+    Account acc;
+    std::stringstream input("JohnDoe\n1\n");
+    std::streambuf* old_cin = std::cin.rdbuf(input.rdbuf());
     acc.userRead();
+    std::cin.rdbuf(old_cin);
+}
+
+TEST(AccountTest, PremiumTextTest) {
+    Account acc;
+    std::stringstream ss;
+    acc.premiumText();
+    std::string output = ss.str();
+    EXPECT_NE(output.find("Discover the advantages of premium!"), std::string::npos);
+}
+
+TEST(AccountTest, InputDataTest) {
+    Account acc;
+    std::stringstream input("1234567890123456 JohnDoe 123\n");
+    std::streambuf* old_cin = std::cin.rdbuf(input.rdbuf());
+    acc.inputData();
+    std::cin.rdbuf(old_cin);
+}
+
+TEST(AccountTest, InputVisualTest) {
+    Account acc;
+    EXPECT_EQ(acc.inputVisual(1), "Premium plus");
+}
+
+TEST(AccountTest, UpgradeTest) {
+    Account acc;
+    std::stringstream input("1\n");
+    std::streambuf* old_cin = std::cin.rdbuf(input.rdbuf());
+    acc.upgrade();
+    std::cin.rdbuf(old_cin);
+}
+
+TEST(AccountTest, SaveTest) {
+    Account acc("Alice", "pass123");
+    acc.save();
+    Verificăm dacă fișierul date.txt conține datele corecte
+}
+
+TEST(AccountTest, AssignmentOperatorTest) {
+    Account original("Bob", "pass123");
+    Account copy;
+    copy = original;
+    EXPECT_EQ(copy.getName(), "Bob");
 }
 
 TEST(AccountTest, OutputOperatorTest) {
-    Account acc("username", "password");
-    std::ostringstream oss;
-    oss << acc; 
-    std::string output = oss.str();
-    EXPECT_TRUE(output.find("username") != std::string::npos); 
+    Account acc("Eve", "mysecret");
+    std::stringstream ss;
+    ss << acc;
+    std::string output = ss.str();
+    EXPECT_NE(output.find("Eve"), std::string::npos);
 }
 
 TEST(AccountTest, InputOperatorTest) {
     Account acc;
-    std::istringstream iss("newusername\nnewpassword\n");
-    iss >> acc; 
-    EXPECT_EQ(acc.getName(), "newusername"); 
-}
-
-TEST(AccountTest, SaveTest) {
-    Account acc("username", "password");
-    acc.save(); 
+    std::stringstream ss("John Jane\npass123\n");
+    ss >> acc;
+    EXPECT_EQ(acc.getName(), "John");
 }
