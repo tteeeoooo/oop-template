0dc46151f671e77bebed56ef3361eaacd32bdc26
tteeeoooo <147204778+tteeeoooo@users.noreply.github.com>
2024-04-20 12:14:48 +0300

    Update noalc_test.cpp

--- test/example/noalc_test.cpp
+++ test/example/noalc_test.cpp
@@ -6,3 +6,12 @@ TEST(NoAlcTest, PriceModifierTest) {
     NoAlc drinkity("Soda", 2.5, 100);
     EXPECT_DOUBLE_EQ(drinkity.priceModifier(), 2.975);
 }
+
+TEST(NoAlcTest, DescriptionTest) {
+    NoAlc drink("Mocktail", 5.0, 50);
+    std::stringstream ss;
+    std::streambuf* oldCout = std::cout.rdbuf(ss.rdbuf()); // Redirect cout to stringstream
+    drink.description();
+    std::cout.rdbuf(oldCout); // Reset cout redirection
+    EXPECT_EQ(ss.str(), "This drink contains 50 calories!\n");
+}
