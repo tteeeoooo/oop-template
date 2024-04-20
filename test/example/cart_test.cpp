c6c6261ced57f0a739b153800b865bfa46762c3a
tteeeoooo <147204778+tteeeoooo@users.noreply.github.com>
2024-04-20 13:04:24 +0300

    Update cart_test.cpp

--- test/example/cart_test.cpp
+++ test/example/cart_test.cpp
@@ -4,15 +4,10 @@
 #include "noalc.h"
 #include "withalc.h"
 
-TEST(CartTest, CartPriceNonEmptyTest) {
-    // Create some Drink objects for the cart
-    Drink* drink1 = new Drink("Coffee", 2.5);
-    Drink* drink2 = new Drink("Tea", 3.0);
-    std::vector<Drink*> drinks = {drink1, drink2};
-
-    // Create a Cart object with the drinks
-    Cart cart(drinks, 5.5, {2.5, 3.0});
+TEST(CartTest, CartPriceEmptyTest) {
+    // Create an empty cart
+    Cart cart({}, 0, {});
 
     // Test the cartPrice function
-    EXPECT_FLOAT_EQ(cart.cartPrice(), 5.5);
+    EXPECT_FLOAT_EQ(cart.cartPrice(), 0);
 }
