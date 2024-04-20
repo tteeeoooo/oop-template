65c159b97197479f0c4dc4fd376516eb070dfff8
tteeeoooo <147204778+tteeeoooo@users.noreply.github.com>
2024-04-20 10:52:24 +0300

    Update cart_test.cpp

--- test/example/cart_test.cpp
+++ test/example/cart_test.cpp
@@ -1,15 +1,15 @@
 #include <gtest/gtest.h>
 #include "cart.h" 
 
-TEST(CartTest, OrderCheckout) {
-    Cart cart;
-    std::vector<Drink*> coffeeMenu; // Presupunând că aceasta este o listă validă de băuturi
+// TEST(CartTest, OrderCheckout) {
+//     Cart cart;
+//     std::vector<Drink*> coffeeMenu; // Presupunând că aceasta este o listă validă de băuturi
 
-    // Simulăm un input care duce la checkout direct
-    std::stringstream input_stream("0\n");
-    std::cin.rdbuf(input_stream.rdbuf());
+//     // Simulăm un input care duce la checkout direct
+//     std::stringstream input_stream("0\n");
+//     std::cin.rdbuf(input_stream.rdbuf());
 
-    // Apelăm funcția order() și verificăm dacă returnează prețul corect (0 pentru un coș gol)
-    EXPECT_EQ(cart.order(cart, coffeeMenu), 0);
-}
+//     // Apelăm funcția order() și verificăm dacă returnează prețul corect (0 pentru un coș gol)
+//     EXPECT_EQ(cart.order(cart, coffeeMenu), 0);
+// }
 
