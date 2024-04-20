ea24226da2209a621d813d75c5acb32e67ee3b73
tteeeoooo <147204778+tteeeoooo@users.noreply.github.com>
2024-04-20 12:35:35 +0300

    Update cart_test.cpp

--- test/example/cart_test.cpp
+++ test/example/cart_test.cpp
@@ -1,19 +1,17 @@
 #include <gtest/gtest.h>
 #include "cart.h"
 
-TEST(CartTest, CreditCardInfoTest) {
+TEST(CartTest, ProductDeleteTest) {
     // Create a cart with some drinks
     std::vector<Drink*> drinks;
     drinks.push_back(new Drink("Coffee", 2.5));
     drinks.push_back(new Drink("Tea", 3.0));
     Cart cart(drinks, 5.5, {2.5, 3.0});
 
-    // Redirect cin to stringstream for testing user input
-    std::stringstream ss("1234567890\n123\n0\n");
-    std::streambuf* oldCin = std::cin.rdbuf(ss.rdbuf());
-    cart.creditCardInfo(cart);
-    std::cin.rdbuf(oldCin); // Reset cin redirection
+    // Delete a product from the cart
+    cart.productDelete(1);
 
-    // Check if the tip calculation is correct
-    EXPECT_FLOAT_EQ(cart.cartPrice(), 5.5);
+    // Check if the price and number of drinks are updated correctly
+    EXPECT_FLOAT_EQ(cart.cartPrice(), 3.0);
+    EXPECT_EQ(cart.getDrinks(cart).size(), 1);
 }
