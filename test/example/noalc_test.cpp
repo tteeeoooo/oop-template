70c14989d772b092ee2639b1c6fd1ed733dff0e0
tteeeoooo <147204778+tteeeoooo@users.noreply.github.com>
2024-04-20 11:36:30 +0300

    Update noalc_test.cpp

--- test/example/noalc_test.cpp
+++ test/example/noalc_test.cpp
@@ -1,16 +1,10 @@
 #include <gtest/gtest.h>
 #include "noalc.h"
 
-TEST(SuiteName, OutputOperatorTest) {
-    // Cream un obiect de tip NoAlc cu anumite valori
+TEST(SuiteName, TestName) {
+    // Aici adaugăm codul pentru testarea operatorului <<
     NoAlc drink("Soda", 2.5, 100);
-
-    // Cream un std::ostringstream pentru a captura ieșirea
     std::ostringstream oss;
-    
-    // Folosim operatorul << pentru a scrie obiectul în std::ostringstream
     oss << drink;
-
-    // Verificăm dacă ieșirea obținută este cea așteptată
     EXPECT_EQ(oss.str(), "Soda 2.5 100");
 }
