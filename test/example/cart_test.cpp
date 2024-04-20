3116c673418d89cf54ff64bc76d1bbf51c71f632
tteeeoooo <147204778+tteeeoooo@users.noreply.github.com>
2024-04-20 13:08:58 +0300

    Update cart_test.cpp

--- test/example/cart_test.cpp
+++ test/example/cart_test.cpp
@@ -1,13 +1,1 @@
-#include <gtest/gtest.h>
-#include "cart.h"
-// #include "drink.h"
-// #include "noalc.h"
-// #include "withalc.h"
 
-TEST(CartTest, CartPriceEmptyTest) {
-    // Create an empty cart
-    Cart cart({}, 0, {});
-
-    // Test the cartPrice function
-    EXPECT_FLOAT_EQ(cart.cartPrice(), 0);
-}
