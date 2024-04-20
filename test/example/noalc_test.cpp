873c7b5b7c39c7c53da6192b85a87130d80bbd2c
tteeeoooo <147204778+tteeeoooo@users.noreply.github.com>
2024-04-20 11:40:10 +0300

    Update noalc_test.cpp

--- test/example/noalc_test.cpp
+++ test/example/noalc_test.cpp
@@ -4,7 +4,7 @@
 // Testăm funcția getCalories()
 TEST(SuiteName, TestName) {
     // Cream un obiect de tip NoAlc cu anumite valori
-    NoAlc drink("Soda", 2.5, 100);
+    NoAlc drinkity("Soda", 2.5, 100);
 
     // Verificăm dacă funcția getCalories() returnează valoarea corectă
     EXPECT_EQ(drink.getCalories(), 100);
