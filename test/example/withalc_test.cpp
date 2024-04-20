580c32a595d047c17683f5b8b249326e6ab39ef5
tteeeoooo <147204778+tteeeoooo@users.noreply.github.com>
2024-04-20 09:38:33 +0300

    Update withalc_test.cpp

--- test/example/withalc_test.cpp
+++ test/example/withalc_test.cpp
@@ -1,60 +1,43 @@
-#include "withalc.h" 
 #include <gtest/gtest.h>
+#include "withalc.h"
 
-TEST(WithAlcTest, ConstructorTest) {
-    WithAlc drink("Beer", 3.0, 5);
-    EXPECT_EQ(drink.getDrinkName(), "Beer");
-    EXPECT_DOUBLE_EQ(drink.getDrinkPrice(), 3.0);
-    EXPECT_EQ(drink.getAlc(), 5);
+TEST(WithAlcTest, Constructor) {
+    WithAlc drink("Whiskey", 10.0, 40);
+    EXPECT_EQ(drink.getDrinkName(), "Whiskey");
+    EXPECT_DOUBLE_EQ(drink.getDrinkPrice(), 10.0);
+    EXPECT_EQ(drink.getAlc(), 40);
 }
 
-TEST(WithAlcTest, CopyConstructorTest) {
-    WithAlc original("Wine", 5.0, 10);
-    WithAlc copy(original);
-    EXPECT_EQ(copy.getDrinkName(), "Wine");
-    EXPECT_DOUBLE_EQ(copy.getDrinkPrice(), 5.0);
-    EXPECT_EQ(copy.getAlc(), 10);
+TEST(WithAlcTest, Description) {
+    WithAlc drink("Vodka", 5.0, 35);
+    testing::internal::CaptureStdout();
+    drink.description();
+    std::string output = testing::internal::GetCapturedStdout(); 
+    EXPECT_NE(output.find("This drink contains alcohol in a percentage of 35%"), std::string::npos);
 }
 
-TEST(WithAlcTest, DescriptionTest) {
-    EXPECT_EQ(drink.description(), "Descrierea așteptată");
+TEST(WithAlcTest, PriceModifier) {
+    WithAlc drink("Rum", 8.0, 25);
+    EXPECT_DOUBLE_EQ(drink.priceModifier(), 9.88); // Pretul cu 19% TVA și acciză adăugate
 }
 
-TEST(WithAlcTest, PriceModifierTest) {
-    EXPECT_DOUBLE_EQ(drink.priceModifier(), 3.0);
+TEST(WithAlcTest, AgeCheck) {
+    EXPECT_TRUE(WithAlc::ageCheck());
 }
 
-TEST(WithAlcTest, AgeCheckTest) {
+TEST(WithAlcTest, AssignmentOperator) {
+    WithAlc drink1("Gin", 6.0, 30);
+    WithAlc drink2;
+    drink2 = drink1;
+    EXPECT_EQ(drink2.getDrinkName(), "Gin");
+    EXPECT_DOUBLE_EQ(drink2.getDrinkPrice(), 6.0);
+    EXPECT_EQ(drink2.getAlc(), 30);
 }
 
-TEST(WithAlcTest, OutputOperatorTest) {
-    WithAlc drink("Whiskey", 8.0, 40);
-    std::stringstream ss;
-    ss << drink;
-    std::string output = ss.str();
-    EXPECT_NE(output.find("Whiskey"), std::string::npos);
-    EXPECT_NE(output.find("8.0"), std::string::npos);
-    EXPECT_NE(output.find("40"), std::string::npos);
-}
-
-TEST(WithAlcTest, InputOperatorTest) {
-    WithAlc drink;
-    std::stringstream ss("Vodka\n6.0\n50\n");
-    ss >> drink;
-    EXPECT_EQ(drink.getDrinkName(), "Vodka");
-    EXPECT_DOUBLE_EQ(drink.getDrinkPrice(), 6.0);
-    EXPECT_EQ(drink.getAlc(), 50);
-}
-
-TEST(WithAlcTest, AssignmentOperatorTest) {
-    WithAlc original("Rum", 7.0, 30);
-    WithAlc copy;
-    copy = original;
-    EXPECT_EQ(copy.getDrinkName(), "Rum");
-    EXPECT_DOUBLE_EQ(copy.getDrinkPrice(), 7.0);
-    EXPECT_EQ(copy.getAlc(), 30);
-}
-
-TEST(WithAlcTest, DestructorTest) {
-    ASSERT_NO_THROW(delete drink);
+TEST(WithAlcTest, OutputOperator) {
+    WithAlc drink("Beer", 3.5, 5);
+    testing::internal::CaptureStdout(); 
+    std::cout << drink; 
+    std::string output = testing::internal::GetCapturedStdout(); 
+    EXPECT_NE(output.find("Beer 3.5 5"), std::string::npos);
 }
