8b4462c88f762a3fcdbb113e7fee9162a2040885
tteeeoooo <147204778+tteeeoooo@users.noreply.github.com>
2024-04-20 11:56:51 +0300

    Update withalc_test.cpp

--- test/example/withalc_test.cpp
+++ test/example/withalc_test.cpp
@@ -1,1 +1,8 @@
+#include <gtest/gtest.h>
+#include "drink.h"
+#include "withalc.h"
 
+TEST(NoAlcTest, PriceModifierTest) {
+    NoAlc drinkity("Wine", 25, 9);
+    EXPECT_DOUBLE_EQ(drinkity.priceModifier(), 31.2375);
+}
