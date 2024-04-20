f85e65eb5f7eca2b1942ffc6869a1d7aceb8d815
tteeeoooo <147204778+tteeeoooo@users.noreply.github.com>
2024-04-20 11:00:10 +0300

    Update cout_test.cpp

--- test/example/cout_test.cpp
+++ test/example/cout_test.cpp
@@ -1,1 +1,19 @@
+#include <gtest/gtest.h>
+#include "cout.h"
 
+TEST(CoutTest, CoutFinishedOrder) {
+    // Testăm funcția statică void coutFinishedOrder() din clasa Cout
+    
+    // Redirecționăm ieșirea standard (cout) către un stringstream pentru a putea verifica mesajul afișat
+    std::stringstream output;
+    std::streambuf* oldCout = std::cout.rdbuf(output.rdbuf());
+
+    // Apelăm funcția coutFinishedOrder()
+    Cout::coutFinishedOrder();
+
+    // Verificăm dacă mesajul afișat este corect
+    EXPECT_EQ(output.str(), "Thank you for your order! We are grinding your coffee right now, to make sure that you will have it as soon as possible! :)");
+
+    // Resetăm ieșirea standard la valoarea anterioară
+    std::cout.rdbuf(oldCout);
+}
