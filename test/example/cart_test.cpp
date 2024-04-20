a3e51f565db7f971e167ad076e7d2730e75faf74
tteeeoooo <147204778+tteeeoooo@users.noreply.github.com>
2024-04-20 12:33:36 +0300

    Update cart_test.cpp

--- test/example/cart_test.cpp
+++ test/example/cart_test.cpp
@@ -1,17 +1,14 @@
 #include <gtest/gtest.h>
 #include "cart.h"
 
-TEST(CartTest, ProductDeleteTest) {
+TEST(CartTest, CartPriceTest) {
     // Create a cart with some drinks
     std::vector<Drink*> drinks;
     drinks.push_back(new Drink("Coffee", 2.5));
     drinks.push_back(new Drink("Tea", 3.0));
     Cart cart(drinks, 5.5, {2.5, 3.0});
 
-    // Delete a product from the cart
-    cart.productDelete(1);
-
-    // Check if the price and number of drinks are updated correctly
-    EXPECT_FLOAT_EQ(cart.cartPrice(), 3.0);
-    EXPECT_EQ(cart.getDrinks(cart).size(), 1);
+    // Check if the cart price is calculated correctly
+    EXPECT_FLOAT_EQ(cart.cartPrice(), 5.5);
 }
+
