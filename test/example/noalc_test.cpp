04ea72851dfe6984ad3f5bdcd7d57ff067f98002
tteeeoooo <147204778+tteeeoooo@users.noreply.github.com>
2024-04-20 11:51:26 +0300

    Update noalc_test.cpp

--- test/example/noalc_test.cpp
+++ test/example/noalc_test.cpp
@@ -1,10 +1,11 @@
 #include <gtest/gtest.h>
 #include "noalc.h"
 
-TEST(NoAlcTest, PriceModifierTest) {
-    // Cream un obiect de tip NoAlc cu anumite valori
-    NoAlc drink("Coffee", 3.0, 120);
-
-    // Verificăm dacă funcția priceModifier() calculează prețul corect
-    EXPECT_DOUBLE_EQ(drink.priceModifier(), 3.57);
+TEST(NoAlcTest, AssignmentOperatorTest) {
+    NoAlc drink1("Soda", 2.5, 100);
+    NoAlc drink2("Cola", 3.0, 150);
+    drink1 = drink2;
+    EXPECT_EQ(drink1.getDrinkChoice(), "Cola");
+    EXPECT_DOUBLE_EQ(drink1.getPrice(), 3.0);
+    EXPECT_EQ(drink1.getCalories(), 150);
 }
