dc188abec508049ceeb11e623afd4eb8c315caf3
tteeeoooo <147204778+tteeeoooo@users.noreply.github.com>
2024-04-20 09:33:37 +0300

    Update drink_test.cpp

--- test/example/drink_test.cpp
+++ test/example/drink_test.cpp
@@ -1,35 +1,43 @@
-#include "Drink.h"
 #include <gtest/gtest.h>
+#include "drink.h"
 
-TEST(DrinkTest, ConstructorTest) {
-    Drink drink("Cappuccino", 3.5);
-    EXPECT_EQ(drink.getDrinkName(), "Cappuccino");
+TEST(DrinkTest, Constructor) {
+    Drink drink("Coffee", 3.5);
+    EXPECT_EQ(drink.getDrinkName(), "Coffee");
     EXPECT_DOUBLE_EQ(drink.getDrinkPrice(), 3.5);
 }
 
-TEST(DrinkTest, CopyConstructorTest) {
-    Drink original("Latte", 4.0);
-    Drink copy(original);
-    EXPECT_EQ(copy.getDrinkName(), "Latte");
-    EXPECT_DOUBLE_EQ(copy.getDrinkPrice(), 4.0);
+TEST(DrinkTest, AssignmentOperator) {
+    Drink drink1("Tea", 2.0);
+    Drink drink2;
+    drink2 = drink1;
+    EXPECT_EQ(drink2.getDrinkName(), "Tea");
+    EXPECT_DOUBLE_EQ(drink2.getDrinkPrice(), 2.0);
 }
 
-TEST(DrinkTest, AssignmentOperatorTest) {
-    Drink original("Espresso", 2.5);
-    Drink copy;
-    copy = original;
-    EXPECT_EQ(copy.getDrinkName(), "Espresso");
-    EXPECT_DOUBLE_EQ(copy.getDrinkPrice(), 2.5);
+TEST(DrinkTest, MenuOptions) {
+    Drink drink1("Coffee", 3.5);
+    Drink drink2("Tea", 2.0);
+    std::vector<Drink*> menu = {&drink1, &drink2};
+    testing::internal::CaptureStdout(); 
+    Drink::menuOptions(menu);
+    std::string output = testing::internal::GetCapturedStdout(); 
+    EXPECT_NE(output.find("Here are your options:"), std::string::npos);
+    EXPECT_NE(output.find("Product 1: Coffee - price with 19% TVA: 3.5"), std::string::npos);
+    EXPECT_NE(output.find("Product 2: Tea - price with 19% TVA: 2"), std::string::npos);
 }
 
-TEST(DrinkTest, DescriptionTest) {
-    EXPECT_EQ(drink.description(), "Descrierea așteptată");
+TEST(DrinkTest, PriceModifier) {
+    Drink drink("Water", 1.0);
+    EXPECT_DOUBLE_EQ(drink.priceModifier(), 1.0);
 }
 
-TEST(DrinkTest, PriceModifierTest) {
-    EXPECT_DOUBLE_EQ(drink.priceModifier(), 3.0);
+TEST(DrinkTest, GetDrinkName) {
+    Drink drink("Soda", 2.5);
+    EXPECT_EQ(drink.getDrinkName(), "Soda");
 }
 
-TEST(DrinkTest, DestructorTest) {
-    ASSERT_NO_THROW(delete drink);
+TEST(DrinkTest, GetDrinkPrice) {
+    Drink drink("Juice", 2.0);
+    EXPECT_DOUBLE_EQ(drink.getDrinkPrice(), 2.0);
 }
