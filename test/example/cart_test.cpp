03d638c6fd39e8c208cc7179a1b0dcdb46ca50cd
tteeeoooo <147204778+tteeeoooo@users.noreply.github.com>
2024-04-20 12:41:00 +0300

    Update cart_test.cpp

--- test/example/cart_test.cpp
+++ test/example/cart_test.cpp
@@ -1,16 +1,24 @@
 #include <gtest/gtest.h>
 #include "cart.h"
 
+int mockInput(const std::string& input) {
+    std::istringstream iss(input);
+    std::cin.rdbuf(iss.rdbuf());
+    int value;
+    std::cin >> value;
+    return value;
+}
+
 TEST(CartTest, GiveATipTest) {
     // Se creează un meniu de băuturi de test
     std::vector<Drink*> coffeeMenu = {new NoAlc("TestDrink1", 5.0), new NoAlc("TestDrink2", 7.0)};
     Cart cart({}, 10.0, {});
 
-    // Se verifică că se calculează corect procentul pentru bacșiș
+    // Simularea introducerii de la tastatură: se introduce 2 pentru a reprezenta 20% pentru bacșiș
+    int userInput = mockInput("2\n");
+
+    // Verificarea că se calculează corect procentul pentru bacșiș
     testing::internal::CaptureStdout();
-    testing::internal::CaptureStderr();
-    std::istringstream input("2"); // Se introduce 20% pentru bacșiș
-    std::cin.rdbuf(input.rdbuf());
     cart.giveATip(cart);
     std::string output = testing::internal::GetCapturedStdout();
     EXPECT_EQ(output, "You will have to pay: 12\n");
