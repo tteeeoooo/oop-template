a47bf2d19a3eaa15eaf488d16a46a27fe690e942
tteeeoooo <147204778+tteeeoooo@users.noreply.github.com>
2024-04-20 11:53:10 +0300

    Update noalc_test.cpp

--- test/example/noalc_test.cpp
+++ test/example/noalc_test.cpp
@@ -1,11 +1,8 @@
 #include <gtest/gtest.h>
+#include "drink.h"
 #include "noalc.h"
 
-TEST(NoAlcTest, AssignmentOperatorTest) {
-    NoAlc drink1("Soda", 2.5, 100);
-    NoAlc drink2("Cola", 3.0, 150);
-    drink1 = drink2;
-    EXPECT_EQ(drink1.getDrinkChoice(), "Cola");
-    EXPECT_DOUBLE_EQ(drink1.getPrice(), 3.0);
-    EXPECT_EQ(drink1.getCalories(), 150);
+TEST(NoAlcTest, PriceModifierTest) {
+    NoAlc drinkity("Soda", 2.5, 100);
+    EXPECT_DOUBLE_EQ(drinkity.priceModifier(), 2.975);
 }
