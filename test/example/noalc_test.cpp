3aec84f2abd2731e16f306ef19b23b70eb0df454
tteeeoooo <147204778+tteeeoooo@users.noreply.github.com>
2024-04-20 11:28:39 +0300

    Update noalc_test.cpp

--- test/example/noalc_test.cpp
+++ test/example/noalc_test.cpp
@@ -2,17 +2,15 @@
 #include "noalc.h" // Presupunând că aici este inclus fișierul header pentru clasa NoAlc
 
 TEST(NoAlcTest, PriceModifier) {
-    // Testăm funcția priceModifier() din clasa NoAlc
-
     // Inițializăm un obiect de tip NoAlc cu un preț specific
     NoAlc noAlc("Non-alcoholic Drink", 100.0, 200); // Presupunând că există un constructor corespunzător în clasa NoAlc
-
-    // Calculăm prețul așteptat pentru testare
-    double expectedPrice = 100.0 + (100.0 * 19) / 100;
 
     // Obținem prețul folosind funcția priceModifier() din clasa NoAlc
     double result = noAlc.priceModifier();
 
-    // Verificăm dacă prețul întors de funcție este corect
-    EXPECT_EQ(result, expectedPrice);
+    // Calculăm prețul așteptat pentru testare
+    double expectedPrice = 100.0 + (100.0 * 19) / 100;
+
+    // Verificăm dacă prețul întors de funcție este cel așteptat
+    EXPECT_DOUBLE_EQ(result, expectedPrice);
 }
