694046115a99ec47a3afc571245c39703a913f94
tteeeoooo <147204778+tteeeoooo@users.noreply.github.com>
2024-04-20 09:49:11 +0300

    Update account_test.cpp

--- test/example/account_test.cpp
+++ test/example/account_test.cpp
@@ -11,6 +11,18 @@ TEST(AccountTest, InputVisual) {
     EXPECT_EQ(account.inputVisual(0), "abort mission");
 }
 
+TEST(AccountTest, Exc) {
+    Account account;
+    std::stringstream input_stream("1\n2\n3\n");
+    std::cin.rdbuf(input_stream.rdbuf()); 
+
+    EXPECT_EQ(account.exc(), 1); 
+    EXPECT_EQ(account.exc(), 2); 
+
+    ASSERT_THROW(account.exc(), std::invalid_argument);
+}
+
+
 // #include <gtest/gtest.h>
 // #include "account.h"
 
