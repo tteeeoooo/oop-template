65afc9df23097a79bc845952821b85a6fbf80ba7
tteeeoooo <147204778+tteeeoooo@users.noreply.github.com>
2024-04-20 12:44:08 +0300

    Update cart_test.cpp

--- test/example/cart_test.cpp
+++ test/example/cart_test.cpp
@@ -6,14 +6,11 @@ TEST(CartTest, GiveATipTest) {
     std::vector<Drink*> coffeeMenu = {new NoAlc("TestDrink1", 5.0), new NoAlc("TestDrink2", 7.0)};
     Cart cart({}, 10.0, {});
 
-    // Simularea introducerii de la tastatură: se introduce 2 pentru a reprezenta 20% pentru bacșiș
-    int userInput = 2;
+    // Se apelează funcția giveATip și se obține valoarea bacșișului pentru 20%
+    float tipAmount = cart.calculateTip(20);
 
-    // Se apelează funcția giveATip și se verifică rezultatul
-    testing::internal::CaptureStdout();
-    cart.giveATip(cart);
-    std::string output = testing::internal::GetCapturedStdout();
-    EXPECT_EQ(output, "You will have to pay: 12\n");
+    // Se verifică că valoarea bacșișului este corect calculată
+    EXPECT_EQ(tipAmount, 2.0);
 
     // Se eliberează memoria pentru băuturi
     for (auto drink : coffeeMenu) {
