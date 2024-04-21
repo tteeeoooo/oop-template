c5910aa3ccd61f03c3d5b19821aa1cf9e8e5f922
tteeeoooo <147204778+tteeeoooo@users.noreply.github.com>
2024-04-21 18:56:59 +0300

    Update drink_test.cpp

--- test/example/drink_test.cpp
+++ test/example/drink_test.cpp
@@ -2,27 +2,13 @@
 #include "drink.h" // Presupunând că aici este inclus fișierul header pentru clasa Drink
 
 TEST(DrinkTest, MenuOptions) {
-    // Testăm funcția menuOptions() din clasa Drink
-
-    // Inițializăm vectorul de băuturi pentru meniu
     std::vector<Drink*> coffeeMenu;
 
-    // Adăugăm câteva băuturi în meniu (presupunem că sunt inițializate corect)
-    // coffeeMenu.push_back(/* Obiect de tip Drink */);
-    // coffeeMenu.push_back(/* Obiect de tip Drink */);
-    // coffeeMenu.push_back(/* Obiect de tip Drink */);
-
-    // Redirecționăm ieșirea standard (cout) către un stringstream pentru a putea verifica mesajele afișate
     std::stringstream output;
     std::streambuf* oldCout = std::cout.rdbuf(output.rdbuf());
 
-    // Apelăm funcția menuOptions() pentru a afișa meniul
     Drink::menuOptions(coffeeMenu);
 
-    // Verificăm dacă mesajele afișate sunt corecte
-    // Aici adăugați aserțiuni pentru a verifica mesajele afișate în funcția menuOptions()
-
-    // Resetăm ieșirea standard la valoarea anterioară
     std::cout.rdbuf(oldCout);
 }
 
