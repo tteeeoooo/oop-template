bb9d47b26422a8f5b56b2bf4d5e6ec868ee4fc29
tteeeoooo <147204778+tteeeoooo@users.noreply.github.com>
2024-05-22 19:57:39 +0300

    Update drinkreview.h

--- include/drinkreview.h
+++ include/drinkreview.h
@@ -26,13 +26,13 @@ private:
     std::map<std::string, T> categories;
 
 public:
-    ReviewProduct(std::map<std::string, T> *firstReview) : Review<T>(firstReview) {
+    explicit ReviewProduct(std::map<std::string, T> *firstReview) : Review<T>(firstReview) {
         this->categories["Taste"] = T{};
         this->categories["Packaging"] = T{};
         this->categories["Price-Quality Ratio"] = T{};
     }
 
-    ReviewProduct(const Review<T> &rev) : Review<T>(rev.getReview()) {
+    explicit ReviewProduct(const Review<T> &rev) : Review<T>(rev.getReview()) {
         this->categories["Taste"] = T{};
         this->categories["Packaging"] = T{};
         this->categories["Price-Quality Ratio"] = T{};
@@ -99,37 +99,42 @@ public:
         cout << "First of all, please grade the taste of the product: " << endl;
         std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
         cin >> opinie;
-        if constexpr (std::is_arithmetic<T>::value) {
-            if (opinie < 0 || opinie > 5) {
-                review->checkGrade(opinie);
-            }
-            categories["Taste"] = opinie;
-//            n1 = float(opinie);
-        }
+        categories["Taste"] = opinie;
+
+//        if constexpr (std::is_arithmetic<T>::value) {
+//            if (opinie < 0 || opinie > 5) {
+//                review->checkGrade(opinie);
+//            }
+//            categories["Taste"] = opinie;
+////            n1 = float(opinie);
+//        }
 
         cout << "Now let us know how you found our packaging. Is it eco-friendly enough and easy to use?" << endl;
         std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
         cin >> opinie;
-        if constexpr (std::is_arithmetic<T>::value) {
-            if (opinie < 0 || opinie > 5) {
-                review->checkGrade(opinie);
-            }
-            categories["Taste"] = opinie;
-//            n2 = float(opinie);
-        }
+        categories["Packaging"] = opinie;
+
+//        if constexpr (std::is_arithmetic<T>::value) {
+//            if (opinie < 0 || opinie > 5) {
+//                review->checkGrade(opinie);
+//            }
+//            categories["Taste"] = opinie;
+////            n2 = float(opinie);
+//        }
 
         cout << "Last but not least, would you say that the product was worth the money?" << endl;
         std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
         cin >> opinie;
-        if constexpr (std::is_arithmetic<T>::value) {
-            if (opinie < 0 || opinie > 5) {
-                review->checkGrade(opinie);
-            }
-            categories["Taste"] = opinie;
-//            n3 = float(opinie);
-//            (*review)[nume] = float(n1 + n2 + n3);
-        }
-        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        categories["Price-Quality Ratio"] = opinie;
+
+//        if constexpr (std::is_arithmetic<T>::value) {
+//            if (opinie < 0 || opinie > 5) {
+//                review->checkGrade(opinie);
+//            }
+//            categories["Taste"] = opinie;
+////            n3 = float(opinie);
+////            (*review)[nume] = float(n1 + n2 + n3);
+//        }
 
         cout << "Thank you for your time! Have a nice day!" << endl;
     };
@@ -137,3 +142,59 @@ public:
 
 #endif // DRINKREVIEW_H
 
+//        cout <<
+//        cin >> review[nume];
+
+//        if (input0 == 1) {
+//            float nota;
+//
+//
+//
+//        }
+
+//        if (stoi(review[nume]) || (checkWords(review[nume]) == true)) {
+//            if (checkWords(review[nume]) == true) {
+//                checkGrade(review[nume]);
+//            }
+//            else {
+//                checkGrade(review[nume]);
+//            }
+//            cout << "Thank you! WOuld you like to go ahead and answer to a few more questions?" << endl;
+//            cout << "Press 1 for yes" << endl;
+//            cout << "Press 0 for no" << endl;
+//            cin >> input2;
+//
+//            if (input2 == 0) {
+//                cout << "Thank you for your time! Hope to see you soon!" << endl;
+//            }
+//            else {
+//                float grade;
+//                cout << "Thank you! This won't take too long, we promise" << endl;
+//                if (stoi(review[nume]) == true) {
+//                    cout << "Please give a grade to the interface of the app: ";
+//                    cin >> grade;
+//                    ReviewProduct::checkGrade(grade);
+//                    review["Interface"] = grade;
+//
+//                    cout << "Now give a grade to the functionality of our appllication: ";
+//                    cin >> grade;
+//                    ReviewProduct::checkGrade(grade);
+//                    review["Functionality"] = grade;
+//
+//                    cout << "Last but not least, please rate how you found the payment process through our app: ";
+//                    cin >> grade;
+//                    ReviewProduct::checkGrade(grade);
+//                    review["Payment"] = grade;
+//
+//                    cout << "You are done! Thank you so much for your help!" << endl;
+//                }
+//            }
+//        }
+//        else if (checkWords(review[nume]) == false) {
+//            cout << "We are sorry, but your review violates our policies." << endl;
+//        }
+//    }
+//
+//
+//
+//}; 
