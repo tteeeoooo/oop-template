5017f5a84ab3a9ea9d91669b79e8e45cc77e191f
tteeeoooo <147204778+tteeeoooo@users.noreply.github.com>
2024-05-22 19:31:16 +0300

    Update appreview.h

--- include/appreview.h
+++ include/appreview.h
@@ -19,12 +19,12 @@ class ReviewApp: public Review<T> {
 private:
     std::map<string, T> caracteristici;
 public:
-    ReviewApp(std::map<std::string, T>* firstReview) : Review<T>(firstReview) {
+    explicit ReviewApp(std::map<std::string, T>* firstReview) : Review<T>(firstReview) {
         this -> caracteristici["Interface"] = T{};
         this -> caracteristici["Functionality"] = T{};
         this -> caracteristici["Payment"] = T{};
     }
-    ReviewApp(const Review<T>& rev) : Review<T>(rev.getReview()) {
+    explicit ReviewApp(const Review<T>& rev) : Review<T>(rev.getReview()) {
         this -> caracteristici["Interface"] = T{};
         this -> caracteristici["Functionality"] = T{};
         this -> caracteristici["Payment"] = T{};
@@ -36,36 +36,41 @@ public:
 //        cout << "Here are other users' reviews:" << endl;
 //        review -> printReviews();
         T opinie;
-        float n1, n2, n3;
+//        float n1, n2, n3;
         cout << "Firstly, please grade our interface: " << endl;
         std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
         cin >> opinie;
-        if constexpr (std::is_arithmetic<T>::value) {
-            if (opinie < 0 || opinie > 5) {
-                review -> checkGrade(opinie);
-            }
-            caracteristici["Interface"] = opinie;
-        }
+        caracteristici["Interface"] = opinie;
+
+//        if constexpr (std::is_arithmetic<T>::value) {
+//            if (opinie < 0 || opinie > 5) {
+//                review -> checkGrade(opinie);
+//            }
+//        }
 
         cout << "What about the overall functionality of the app?" << endl;
         std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
         cin >> opinie;
-        if constexpr (std::is_arithmetic<T>::value) {
-            if (opinie < 0 || opinie > 5) {
-                review -> checkGrade(opinie);
-            }
-            caracteristici["Functionality"] = opinie;
-        }
+        caracteristici["Functionality"] = opinie;
+
+//        if constexpr (std::is_arithmetic<T>::value) {
+//            if (opinie < 0 || opinie > 5) {
+//                review -> checkGrade(opinie);
+//            }
+//            caracteristici["Functionality"] = opinie;
+//
+//        }
 
         cout << "Last but not least, what would you say about our payment system?" << endl;
         std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
         cin >> opinie;
-        if constexpr (std::is_arithmetic<T>::value) {
-            if (opinie < 0 || opinie > 5) {
-                review -> checkGrade(opinie);
-            }
-            caracteristici["Payment"] = opinie;
-        }
+        caracteristici["Payment"] = opinie;
+
+//        if constexpr (std::is_arithmetic<T>::value) {
+//            if (opinie < 0 || opinie > 5) {
+//                review -> checkGrade(opinie);
+//            }
+//        }
 
         cout << "Thank you for your time! Your review will help us improve our services in the future! Have a nice day!" << endl;
     }
@@ -79,3 +84,18 @@ public:
 
 #endif //OOP_APPREVIEW_H
 
+
+//else if (input == 2) {
+//string parere;
+//            cout << "First of all, please tell us a few words about your overall impression of our app:" << endl;
+//            cin >> parere;
+//
+//            cout << "Now please tell us your impression of the interface of the app: is it easy to use? Do you like how it looks?" << endl;
+//            std::getline(caracteristici["Interface"]);
+//
+//            cout << "What about the overall functionality of the app?" << endl;
+//            std::getline(caracteristici["Functionality"]);
+//
+//            cout << "Lastly, what would you say about our payment system?" << endl;
+//            std::getline(caracteristici["Payment"]);
+//}
